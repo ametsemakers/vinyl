ccade1e1a6b63edb4f48e3049a7c436481a9f5bf
Alex Metsemakers <a_metsemakers@hotmail.com>
2020-02-07 17:03:03 +0100

    Update layout.php

--- mvc/App/Backend/Templates/layout.php
+++ mvc/App/Backend/Templates/layout.php
@@ -24,7 +24,7 @@
 
             <!-- FLASH MESSAGE -->
             <div class="row">
-                <div class="col col-md-12">
+                <div class="col col-md-12" id="alert_message">
                     <?php if ($user->hasFlash()) echo '<p style="text-align: center;">', $user->getFlash(), '</p>'; ?>
                 </div>
             </div>
@@ -41,5 +41,31 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script>
+            // ici pour tester, à deplacer dans web
+            var declared;
+            try
+            {
+                alert = $getElementById("alert_message");
+                declared = true;
+            }
+            catch(e)
+            {
+                declared = false;
+            }
+
+            
+            
+             
+            if (declared) 
+            {
+                setTimeout(function()
+                {
+                    $getElementById("alert_message").remove();
+                }, 5000 ); // 5 secs
+            };
+            
+            
+        </script>
     </body>
 </html>
